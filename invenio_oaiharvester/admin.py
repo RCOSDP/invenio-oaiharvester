e25176fbb918fa231471bff37e6daa192dbd8be4
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-04 15:11:02 +0900

    Add admin screen.

--- invenio_oaiharvester/admin.py
+++ invenio_oaiharvester/admin.py
@@ -58,8 +58,9 @@ class HarvestSettingView(ModelView):
         'until_date', 'set_spec', 'metadata_prefix', 'target_index',
         'update_style', 'auto_distribution'
     )
-    # form_choices = dict(
-    #     update_style=RadioField('Style', choices=[('0','Difference'),('1','Bulk')]))
+    form_overrides = dict(
+        update_style=RadioField
+    )
     form_choices = dict(
         update_style=[('0', 'Bulk'), ('1', 'Difference')],
         auto_distribution=[('0', 'Run'), ('1', 'Do not run')])
