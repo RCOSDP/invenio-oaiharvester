06deb75a8dccba5ebefe71a77350b2f858fc8ddc
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-04 17:16:08 +0900

    Add admin screen.

--- invenio_oaiharvester/models.py
+++ invenio_oaiharvester/models.py
@@ -70,17 +70,12 @@ class HarvestSettings(db.Model):
     until_date = db.Column(db.Date, nullable=True)
     set_spec = db.Column(db.String(255), nullable=True)
     metadata_prefix = db.Column(db.String(255), nullable=False)
-    # node_id = db.Column(
-    #     db.BigInteger,
-    #     db.ForeignKey(Index.id),
-    #     nullable=False
-    # )
-
-    target_index = db.relationship(Index,
-                                   backref='target_index',
-                                   foreign_keys=[db.Column(db.BigInteger,
-                                                           db.ForeignKey(Index.id),
-                                                           nullable=False)])
+    index_id = db.Column(
+        db.BigInteger,
+        db.ForeignKey(Index.id),
+        nullable=False
+    )
+    target_index = db.relationship(Index, backref='target_index', foreign_keys=[index_id])
     # target_index = db.Column(db.Integer, nullable=False)
     # update_style = db.Column(db.Integer,
     #     nullable=False,
