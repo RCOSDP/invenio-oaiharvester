9cac7d6cac03dc9f07383f79a013c3f7aa0283af
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-05 15:11:08 +0900

    Add admin screen.

--- invenio_oaiharvester/admin.py
+++ invenio_oaiharvester/admin.py
@@ -80,7 +80,9 @@ class HarvestSettingView(ModelView):
         'target_index',
         'update_style',
         'auto_distribution',
-        rules.HTML('<div class="form-group"><button id="harvesting-btn" class="col-md-offset-2 btn btn-primary">Run</button></div>'),
+        rules.HTML('<div class="form-group"><div class="col-md-2"></div>'
+                   '<div class="col-md-10"><button id="harvesting-btn" '
+                   'class="btn btn-primary">Harvesting</button></div></div>'),
     )
     # form_overrides = dict(newField=SubmitField)
     # form_args = dict(
