ed1517ea6d6b443cb2ceeb15849c678efd3d9c97
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-05 18:48:30 +0900

    Add admin screen.

--- invenio_oaiharvester/admin.py
+++ invenio_oaiharvester/admin.py
@@ -48,9 +48,10 @@ class HarvestSettingView(ModelView):
     from .views import blueprint
     # details_template = current_app.config['OAIHARVESTER_DETAIL_TEMPLATE']
 
-    details_template = os.path.join(blueprint.root_path,
-                                    blueprint.template_folder,
-                                    'admin/details.html')
+    details_template = 'templates/admin/harvest_details.html'
+    # details_template = os.path.join(blueprint.root_path,
+    #                                 blueprint.template_folder,
+    #                                 'templates/admin/harvest_details.html')
 
     # details_template = current_app.config['OAIHARVESTER_DETAIL_TEMPLATE']
     # form_overrides = dict(
