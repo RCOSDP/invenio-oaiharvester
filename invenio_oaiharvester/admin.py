62d428c86b1e0124271c40d5db7ed1523224171a
weko3-dev12 <weko3-hitachi-dev12@ml.itg.hitachi.co.jp>
2019-02-06 11:04:38 +0900

    Add admin screen.

--- invenio_oaiharvester/admin.py
+++ invenio_oaiharvester/admin.py
@@ -65,7 +65,7 @@ class HarvestSettingView(ModelView):
         # Harvesting='<a id="harvesting-btn" class="btn btn-primary" href="#">Run</a>',
         # Harvesting=link('Objects', '#'),
         Harvesting=link('Harvesting', lambda o: url_for(
-            'index')),
+            'HarvestSettings.index_view')),
 
     )
     column_details_list = (
